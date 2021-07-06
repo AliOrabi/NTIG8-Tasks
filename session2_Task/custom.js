c63c4578a88d17555f91f1983fa0d6fbba57cff8
Ali Orabi <17912072+AliOrabi@users.noreply.github.com>
2021-07-06 08:44:07 +0200

    task_1

---
+++ session2_Task/custom.js
@@ -0,0 +1,26 @@
+let formHeads = [
+    'taskTitle',
+     "taskType",
+      "taskContent", 
+      "startDate", 
+      "dueDate"]
+      
+showHide.addEventListener('click', function(e){
+    form_div.classList.toggle('d-none')
+    this.textContent == "Show" ? this.textContent='Hide': this.textContent='Show'
+})
+
+myAddForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    let task = { status:false }
+    formHeads.forEach(h => {
+            task[h]= this.elements[h].value
+    })
+    console.log(task)
+    addTask(task)
+})
+
+myAddForm.addEventListener('click', function(e){
+    form_div.classList.toggle('btn')
+    this.elementTag === 'Update'
+})
